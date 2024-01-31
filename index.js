505a8dcd7389afafe4f78f9db17f50f470deeb4b
Sayuri <sayurikamble123@gmail.com>
2024-01-31 15:52:07 +0530

    web socket setup

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const { createServer } = require("node:http");
+const { Server } = require("socket.io");
+
 const app = express();
+const server = createServer(app);
+console.log(server);
+const io = new Server(server);
 
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-app.listen(5000);
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("join", (room) => {
+    console.log("room", room);
+  });
+});
+
+server.listen(5000, () => console.log(`Listening on port 5000`));
